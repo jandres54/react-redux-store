f611c51bcf7db43ca7ff8bdb5326136f10d885b3
Jorge <78658812+jandres54@users.noreply.github.com>
2021-06-03 10:04:10 -0500

    update

--- react-redux-store/src/App.js
+++ react-redux-store/src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Redux Store</h1>
+      <div>
+        <Router
+      </div>
+
     </div>
   );
 }
---
+++ react-redux-store/src/components/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mensShirt = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default mensShirt;
